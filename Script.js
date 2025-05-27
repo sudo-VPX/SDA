52f0317267c3137f1816814b2ca18c73fb289825
Vikrant Pathania <vikrantpathania.2007@gmail.com>
2025-05-27 17:35:20 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+let Social_Links = {
+    FaceBook: "https://www.facebook.com/share/1EHNuJpwWk/",
+    InstaGram: "https://www.instagram.com/suryadefence_academy_pathankot",
+    YouTube: "https://www.youtube.com/@SURYADEFENCEACADEMYPATHANKOT",
+    WhatsApp: "https://api.whatsapp.com/send/?phone=7355666622"
+
+};
+
+document.getElementById("FaceBook_Link").href = Social_Links.FaceBook;
+document.getElementById("InstaGram_Link").href = Social_Links.InstaGram;
+document.getElementById("YouTube_Link").href = Social_Links.YouTube;
+document.getElementById("WhatsApp_Link").href = Social_Links.WhatsApp;
