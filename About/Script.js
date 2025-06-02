49b5296064eb13e980ad4a7cc779505c816a4817
Vikrant Pathania <vikrantpathania.2007@gmail.com>
2025-06-02 13:04:38 +0530

    Add files via upload

---
+++ About/Script.js
@@ -0,0 +1,15 @@
+let Social_Links = {
+    FaceBook: "https://www.facebook.com/share/1EHNuJpwWk/",
+    InstaGram: "https://www.instagram.com/suryadefence_academy_pathankot",
+    YouTube: "https://www.youtube.com/@SURYADEFENCEACADEMYPATHANKOT",
+    WhatsApp: "https://api.whatsapp.com/send/?phone=7355666622&text=Hi+Sir+I%27m+Interested+To+Join+Academy.+So%2C+I+Want+Some+Details+About+Academy",
+    TeleGram: " "
+
+
+};
+
+document.getElementById("FaceBook_Link").href = Social_Links.FaceBook;
+document.getElementById("InstaGram_Link").href = Social_Links.InstaGram;
+document.getElementById("YouTube_Link").href = Social_Links.YouTube;
+document.getElementById("WhatsApp_Link").href = Social_Links.WhatsApp;
+document.getElementById("TeleGram_Link").href = Social_Links.TeleGram;
